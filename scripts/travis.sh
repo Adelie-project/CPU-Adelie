8c95e5258e054e4f6e4cef1aa7f57b7da6acda37
Raphael Isemann <teemperor@gmail.com>
2017-10-12 16:30:34 -0700

    Now also testing simulator in travis

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -e
 # Navigate to our project dir
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
@@ -7,6 +7,30 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR/.."
 cd simulator/
 make -j2
+
+# Fun each bin file through the simulator and check the command line output.
+for f in test/*.bin
+do
+    # This tests takes really long, let's skip it for now...
+    if [[ $f == "test/fib_rec.bin" ]]; then
+      continue
+    fi
+    echo "Running test $f"
+    test_out="`basename $f`.output"
+    test_expected="test/outputs/`basename $f`.expected"
+    echo "Output is $test_out"
+    # Uncommend this line to update the reference files:
+   #echo "r" | /usr/bin/time -v ./sim "$f" | tee "$test_expected"
+    echo "r" | /usr/bin/time -v ./sim "$f" | tee "$test_out"
+    diff -U1 "$test_out" "$test_expected"
+    if [ $? -eq 0 ]; then
+        echo "Test $f passed"
+    else
+        echo "Test $f failed"
+        exit 1
+    fi
+done
+
 
 # Test build the compiler
 # Enable when we figure out what our min-caml is doing.
