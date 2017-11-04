87daa8cd474f9679607da2da71b4cf71b988668c
mdj982 <maodejun001@hotmail.com>
2017-11-04 16:18:02 +0900

    ADDI~ANDIの範囲外即値の変換に対応

---
+++ simulator/asm_source/irregular.hpp
@@ -0,0 +1,19 @@
+#ifndef IRREGULAR_HPP
+#define IRREGULAR_HPP
+
+#include <sstream>
+#include "asm_globalparam.hpp"
+#include "encoder.hpp"
+
+#define Loop(i, n) for(int i = 0; i < (int)n; i++)
+#define Loop1(i, n) for(int i = 1; i <= (int)n; i++)
+#define Loopr(i, n) for(int i = (int)n - 1; i >= 0; i--)
+#define Loopr1(i, n) for(int i = (int)n; i >= 1; i--)
+
+void divide(param_t *param);
+
+void pre_parce_irregular(param_t* param, string s);
+
+void parce_irregular(param_t* param);
+
+#endif
