0111c622027f473d9c5e345db837e9411927d10c
mdj982 <maodejun001@hotmail.com>
2017-12-03 02:08:12 +0900

    test_io改変

--- simulator/test/test_io.s
+++ simulator/test/test_io.s
@@ -1,3 +1,23 @@
+set %r5, $0x32
+out %r5
+set %r6, $0x58
+out %r6
+set %r7, $0x59
+out %r7
+set %r7, $0x60
+out %r7
+set %r7, $0x61
+out %r7
+set %r7, $0x62
+out %r7
+set %r7, $0x63
+out %r7
+set %r7, $0x64
+out %r7
+set %r7, $0x65
+out %r7
+set %r7, $0x66
+out %r7
 in %r5
 in %r6
 in %r7
@@ -20,3 +40,4 @@ srli %r9, %r9, $8
 out %r9
 srli %r9, %r9, $8
 out %r9
+jal %r0, $0
